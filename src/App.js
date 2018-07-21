7c231b8ef3952855dac1fc022d2a2e4ce555c8ea
haochengz <haochengzhao@outlook.com>
2018-07-21 23:41:35 +0800

    A empty project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
 
-import './App.css';
-import { Add, Rem } from './index.redux'
-
-@connect(
-  state => ({num: state}),
-  { Add, Rem }
-)
-class App extends Component {
+export default class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h2>Now we have {this.props.num} machine guns</h2>
-        <button onClick={this.props.Add}>ADD</button>
-        <button onClick={this.props.Rem}>REMOVE</button>
-      </div>
-    );
+    return <h2>APP</h2>
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-
-import './index.css';
+import registerServiceWorker from './registerServiceWorker'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {
-  machineGunAction
-} from './index.redux'
-
-const reduxDev = window.devToolsExtension() || (f => f)
-const store = createStore(machineGunAction, compose(
-  applyMiddleware(thunk),
-  reduxDev
-))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'));
+  <App />,
+  document.getElementById('root')
+);
 registerServiceWorker();
