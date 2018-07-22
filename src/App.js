9588d8d0cfefce425f941043630e494db05158fd
haochengz <haochengzhao@outlook.com>
2018-07-22 12:11:07 +0800

    config: basic project structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+
+import Front from './views/front'
 
 export default class App extends Component {
   render() {
-    return <h2>APP</h2>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Front}></Route>
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 import registerServiceWorker from './registerServiceWorker'
-import App from './App';
+
+import './config'
+import App from './App'
+import reducers from './storage/reducers'
+
+const store = createStore(reducers, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
