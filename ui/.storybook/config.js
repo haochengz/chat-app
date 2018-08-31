9d23eed13a7f8cc5a3f37b0839c6e343ec74af25
haochengz <haochengzhao@outlook.com>
2018-08-31 16:23:11 +0800

    ui chores: set the mobile device viewport for storybook

--- ui/.storybook/config.js
+++ ui/.storybook/config.js
@@ -1,9 +1,33 @@
 import { configure } from '@storybook/react';
 
+import { configureViewport } from '@storybook/addon-viewport'
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+// const newViewports = {
+//   iphonex: {
+//     name: 'iPhone X',
+//     styles: {
+//       height: '1218px',
+//       width: '563px',
+//     },
+//     type: 'mobile',
+//   }
+// }
+
+// configureViewport({
+//   viewports: {
+//     ...INITAIL_VIEWPORTS,
+//     ...newViewports
+//   }
+// })
+
+// configureViewport({
+//   defaultViewport: 'iphonex'
+// })
 
 configure(loadStories, module);
--- ui/stories/input/username.stories.js
+++ ui/stories/input/username.stories.js
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions';
 
 import UsernameInput from '../../src/components/input/username'
 
+
 storiesOf('UsernameInput', module)
   .add('display', () => (
     <UsernameInput />
