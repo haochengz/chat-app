48b115f9dc7a74a81b9b3fee10186d8b7d8f5279
haochengz <haochengzhao@outlook.com>
2018-08-31 15:34:53 +0800

    ui feat: setup storybook env

---
+++ ui/stories/input/avatar.stories.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import UsernameInput from '../../src/components/input/username'
+
+storiesOf('UsernameInput', module)
+  .add('display', () => (
+    <UsernameInput />
+  ))
